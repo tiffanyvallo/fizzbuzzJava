6e13d290fa0da438ae9c2ddd4353b7ac7c652a3b
tiffanyvallo <tiffanyvallo@tiffanys-mbp.home>
2021-08-26 16:36:08 +0100

    fizz buzz done

--- src/main/java/Fizzbuzz.java
+++ src/main/java/Fizzbuzz.java
@@ -1,2 +1,26 @@
 public class Fizzbuzz {
+
+    public String fizzbuzz(Integer upTo) {
+        String result = "";
+        for (int i = 1; i <= upTo; i++) {
+            if (i == 1) {
+                result = play(i);
+            } else {
+                result += ", " + play(i);
+            }
+        }
+        return result;
+    }
+
+    public static String play(Integer number){
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return number.toString();
+        }
+    }
 }
--- src/test/java/FizzbuzzTest.java
+++ src/test/java/FizzbuzzTest.java
@@ -1,2 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+
 public class FizzbuzzTest {
+    Fizzbuzz subject;
+    @BeforeEach
+    void setUp() {
+        subject = new Fizzbuzz();
+    }
+    @Test
+    void testGenerateListsNumbersUpToThree() {
+        String result = subject.fizzbuzz(3);
+        assertEquals("1, 2, Fizz", result);
+    }
+    @Test
+    void testGenerateListsNumbersUpToFive() {
+        String result = subject.fizzbuzz(5);
+        assertEquals("1, 2, Fizz, 4, Buzz", result);
+    }
+    @Test
+    void testGenerateListsNumbersUpToFifteen() {
+        String result = subject.fizzbuzz(15);
+        assertEquals("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, FizzBuzz", result);
+    }
 }
